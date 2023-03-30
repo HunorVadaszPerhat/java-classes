522bd8df3da1c10089d9240bed693c41228e5a0b
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-03-30 16:48:55 +0200

    added static inner class

---
+++ inner_class/static_inner_class/src/Outer.java
@@ -0,0 +1,13 @@
+public class Outer {
+    static int x = 10;
+    int y = 20;
+
+    static class Inner {
+        void display(){
+            System.out.println("From Outer class, x is: " + x);
+
+            // Non-static field 'y' cannot be referenced from a static context
+            // System.out.println("From Outer class, y is: " + y);
+        }
+    }
+}
