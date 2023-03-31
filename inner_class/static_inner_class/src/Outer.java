d27efc872d23f4fdf14e2df346b774b5faf6939c
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-03-31 17:51:43 +0200

    added comment

--- inner_class/static_inner_class/src/Outer.java
+++ inner_class/static_inner_class/src/Outer.java
@@ -2,6 +2,7 @@ public class Outer {
     static int x = 10;
     int y = 20;
 
+    // You cannot use the static keyword with a class unless it is an inner class
     static class Inner {
         void display(){
             System.out.println("From Outer class, x is: " + x);
