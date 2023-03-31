1346b44948c83a11d206c1aeb8fc23ad88ac34a9
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-03-31 16:55:23 +0200

    static members + blocks
    - added class with static members -> variables, methods
    - added class with static blocks -> for initialising static variable

---
+++ static/static_members/HondaCity.java
@@ -0,0 +1,18 @@
+public class HondaCity {
+
+    // static field
+    static long price = 10;
+    int a, b;
+
+    // static method
+    static double onRoadPrice(String city){
+        switch (city) {
+            case "delhi":
+                return price;
+            case "london":
+                return price;
+            default:
+                return price;
+        }
+    }
+}
